8f6e61f5d9ffa03ac5e4b6f732d59da44bfd634d
IProgramWell <couldntgetemail@mail.com>
2023-04-26 13:04:58 +0300

    Updated build script to always produce the same filename

--- build.sh
+++ build.sh
@@ -4,4 +4,5 @@ npm test && \
 	npx tsc && \
 	cp ./package.json ./build && \
 	npm pack ./build --pack-destination ./dist && \
-	rm -rf ./build
+	rm -rf ./build && \
+	mv "./dist/$npm_package_name-$npm_package_version.tgz" "./dist/$npm_package_name.tgz";
