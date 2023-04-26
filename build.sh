6a96af8995d8c5b2afa0cb2e7423f0aae114a840
IProgramWell <couldntgetemail@mail.com>
2023-04-26 13:16:48 +0300

    Forgot to add a few files

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 npm test && \
 	npx tsc && \
 	cp ./package.json ./build && \
+	cp ./global.d.ts ./build && \
+	cp ./webpackUtils.js ./build && \
+	cp -r ./types ./build && \
+	cp ./README.md ./build && \
+	ls ./build && \
 	npm pack ./build --pack-destination ./dist && \
 	rm -rf ./build && \
 	mv "./dist/$npm_package_name-$npm_package_version.tgz" "./dist/$npm_package_name.tgz";
