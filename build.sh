b4b6db5553f3e2db2d43c64bd264bf26c6845bd8
IProgramWell <couldntgetemail@mail.com>
2023-04-26 13:43:37 +0300

    Hopefully this will add the built package to the repository

--- build.sh
+++ build.sh
@@ -2,12 +2,12 @@
 
 npm test && \
 	npx tsc && \
+	mkdir -p ./dist && \
 	cp ./package.json ./build && \
 	cp ./global.d.ts ./build && \
 	cp ./webpackUtils.js ./build && \
 	cp -r ./types ./build && \
 	cp ./README.md ./build && \
-	mkdir -p ./dist && \
 	npm pack ./build --pack-destination ./dist && \
 	rm -rf ./build && \
 	mv "./dist/$npm_package_name-$npm_package_version.tgz" "./dist/$npm_package_name.tgz";
