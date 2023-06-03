38db06bcb887275a46a3bd24ac1f1d148db58431
IProgramWell <couldntgetemail@mail.com>
2023-06-03 17:13:25 +0300

    Forgot to remove

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,4 @@
 export * as ViolentMonkey from "./ViolentMonkey";
-export * as GeneralTypes from "./GeneralTypes";
 export * as ElementTypes from "./ElementTypes";
 export * as Interfaces from "./Interfaces";
 export * as UtilityTypes from "./UtilityTypes";
